4b9f72d4925af23488dd360f61e88a45b335e5ae
Dan Gross <danwgross@gmail.com>
2022-11-03 18:54:46 -0400

    additional footer adjustments

--- client/src/Pages/Layout/Components/Footer.tsx
+++ client/src/Pages/Layout/Components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 
 const Footer: React.FC<{}> = () => {
   return (
-    <footer className="text-center bg-dark mt-5">
+    <footer className="text-center bg-dark mt-5 flex-grow-0 flex-shrink-0">
       <img src="ttb-logos_white.png" alt="Logo" className={`${styles.Logo}`} />
     </footer>
   );
