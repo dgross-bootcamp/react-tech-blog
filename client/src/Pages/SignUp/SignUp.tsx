a7022a998af6becf66180ef783e676e315ee19ca
Dan Gross <danwgross@gmail.com>
2022-11-03 19:57:05 -0400

    added signup mutation

--- client/src/Pages/SignUp/SignUp.tsx
+++ client/src/Pages/SignUp/SignUp.tsx
@@ -1,9 +1,36 @@
+import { gql, useMutation } from "@apollo/client";
 import React, { FormEvent, useState } from "react";
+import { UserDTO } from "../../types/types";
+import Auth from "../../utils/Auth";
 import styles from "./SignUp.module.css";
+
+interface SignUpMutationVariables {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const SIGN_UP = gql`
+  mutation register($username: String!, $email: String!, $password: String!) {
+    register(username: $username, email: $email, password: $password) {
+      email
+      token
+      username
+      bio
+      image
+    }
+  }
+`;
 
 const SignUp: React.FC<{}> = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const [register] = useMutation<
+    { register: UserDTO },
+    SignUpMutationVariables
+  >(SIGN_UP);
 
   const handleEmailChange = (e: FormEvent<HTMLInputElement>) => {
     setEmail(e.currentTarget.value);
@@ -13,10 +40,30 @@ const SignUp: React.FC<{}> = () => {
     setPassword(e.currentTarget.value);
   };
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleUsernameChange = (e: FormEvent<HTMLInputElement>) => {
+    setUsername(e.currentTarget.value);
+  };
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    try {
+      const response = await register({
+        variables: {
+          email,
+          password,
+          username,
+        },
+      });
+
+      Auth.login(response.data?.register.token!);
+    } catch (e) {
+      console.error(e);
+    }
+
     setEmail("");
     setPassword("");
+    setUsername("");
   };
 
   return (
@@ -25,6 +72,18 @@ const SignUp: React.FC<{}> = () => {
         <form onSubmit={handleFormSubmit}>
           <img src="ttb-logos.jpeg" alt="Logo" className={`${styles.Logo}`} />
           <h1 className="h3 mb-3 fw-normal">Please Sign Up</h1>
+          <div className="form-floating mb-2">
+            <input
+              type="text"
+              value={username}
+              name="username"
+              id="username"
+              placeholder="Username"
+              onChange={handleUsernameChange}
+              className="form-control"
+            />
+            <label htmlFor="username">Username</label>
+          </div>
           <div className="form-floating mb-2">
             <input
               type="email"
